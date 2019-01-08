c1104c5a05e9639984bff0d157e8652059323880
elin.nilsson <elinhejnilsson@gmail.com>
2019-01-08 08:50:09 +0100

    Added some styling and sorting.

--- main.js
+++ main.js
@@ -20,27 +20,51 @@ Vue.component('create-todo', {
     data(){
         return {
             assignment: null,
-            dueDate: null
+            dueDate: null,
+            errors: []
         };
     },
     template: `
+    <div>
+        <ul>
+            <li v-for="error in errors">
+                {{ error }}
+            </li>
+        </ul>
         <form>
             <input type="text"
                    v-model="assignment">
             <input type="date"
                    v-model="dueDate">
-            <primary-button text="Add todo" :button-click="buttonClick"></primary-button>
-        </form>`,
+            <primary-button text="Add todo" :button-click="addTodo"></primary-button>
+        </form>
+    </div>`,    
     methods: {
-        buttonClick(){
+        addTodo(){
             let todo = {};
 
+            if(!this.assignment || !this.dueDate){
+                this.errors = [];
+
+                if(!this.assignment){
+                    this.errors.push('What\'s the assigment?');
+                }
+
+                if(!this.dueDate){
+                    this.errors.push('Please submit a due date');
+                }
+                return;
+            }
+
+            this.errors = [];
             todo.assignment = this.assignment;
             todo.dueDate = this.dueDate;
             todo.finished = false;
             todo.removed = false;
-            // TODO validation
             eventBus.$emit('add-todo', todo)
+
+            this.dueDate = null;
+            this.assignment = null;
         }
     }
 });
@@ -54,12 +78,52 @@ Vue.component('todo-list', {
         }
     },
     template: `
-        <ul>
-            <li v-for="(todo, index) in todos" :key="index">
-                <todo-list-item :todo=todo></todo-list-item>
-            </li>            
-        </ul>
+        <div>
+            <sorting-menu :items=todos></sorting-menu>
+            <ul class="regular-list">
+                <li v-for="(todo, index) in todos" :key="index">
+                    <todo-list-item :todo=todo></todo-list-item>
+                </li>            
+            </ul>
+        </div>
     `
+});
+
+Vue.component('sorting-menu', {
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <div>
+            <primary-button text="Ascending Due Date" :button-click="sortByAscendingDueDate"></primary-button>
+            <primary-button text="Descending Due Date" :button-click="sortByDescendingDueDate"></primary-button>
+        </div>
+    `,
+    methods: {
+        sortByAscendingDueDate(){
+            this.items.sort(function(a, b){ 
+                if(a.dueDate < b.dueDate){
+                    return -1;
+                } else if(b.dueDate < a.dueDate){
+                    return 1;
+                }
+                return 0;
+            });
+        },
+        sortByDescendingDueDate(){
+            this.items.sort(function(a, b){
+                if(a.dueDate > b.dueDate){
+                    return -1;
+                } else if(b.dueDate > a.dueDate){
+                    return 1;
+                }
+                return 0;
+            });
+        }
+    }
 });
 
 Vue.component('todo-list-item', {
@@ -69,15 +133,11 @@ Vue.component('todo-list-item', {
             type: Object
         }
     },
-    template: `<span v-bind:class="{finished: todo.finished, removed: todo.removed}">{{ todo.assignment }} is due {{ todo.dueDate}} 
-    <primary-button text="Done and Done" 
-                    :button-click="finishTodo"></primary-button>
-    <primary-button text="I don't wanna" 
+    template: `<span v-bind:class="{ finished: todo.finished, removed: todo.removed }"> <input type="checkbox"
+    v-model="todo.finished"> {{ todo.assignment }} is due {{ todo.dueDate}} 
+    <primary-button text="Remove" 
                     :button-click="removeTodo"></primary-button></span>`,
     methods: {
-        finishTodo(){
-            this.todo.finished = true;
-        },
         removeTodo(){
             this.todo.removed = true;
         }
@@ -87,9 +147,12 @@ Vue.component('todo-list-item', {
 const app = new Vue({
     el: '#app',
     data: {
-        todos: []        
+        todos: [],
+        errors: []     
     },
     mounted(){
-        eventBus.$on('add-todo', todo => { this.todos.push(todo) });
+        eventBus.$on('add-todo', todo => {             
+            this.todos.push(todo);
+        });
     }
 });
